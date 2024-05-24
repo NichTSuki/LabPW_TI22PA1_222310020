64c37d28000174dde1ccd71c57445850c60eb19a
Nicholas Tony Sukino <113115604+NichTSuki@users.noreply.github.com>
2024-05-24 21:14:08 +0700

    Create Tugas5biodata.blade.php

---
+++ Tugas5biodata.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata Diri</title>
+    <body>
+    <div class="container">
+        <h1>Biodata Diri</h1>
+        <div class="detail">
+            <label for="npm">NPM :</label>
+            <span id="npm">222310020</span>
+        </div>
+        <div class="detail">
+            <label for="nama">Nama :</label>
+            <span id="nama">Nicholas Tony Sukino</span>
+        </div>
+        <div class="detail">
+            <label for="umur">Umur :</label>
+            <span id="umur">20 tahun</span>
+        </div>
+        <div class="detail">
+            <label for="alamat">Alamat :</label>
+            <span id="alamat">Jl. Ence Soemanta Diredja no 30 RT02/RW07</span>
+        </div>
+        <div class="detail">
+            <label for="pekerjaan">Pendidikan :</label>
+            <span id="pekerjaan">Institut Bisnis Dan Informatika Kesatuan</span>
+        </div>
+        <div class="detail">
+            <label for="email">Email :</label>
+            <span id="email">222310020@student.ibik.ac.id</span>
+        </div>
+        <div class="detail">
+            <label for="telepon">No.Telepon :</label>
+            <span id="telepon">08811859230</span>
+        </div>
+        <div class="detail">
+            <label for="jk">Jenis Kelamin :</label>
+            <span id="jk">Pria</span>
+        </div>
+        <div class="detail">
+            <label for="t">Tinggi :</label>
+            <span id="t">170 cm</span>
+        </div>
+        <div class="detail">
+            <label for="b">Berat :</label>
+            <span id="b">45 kg</span>
+        </div>
+    </div>
+</body>
+    <style>
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg8vgUfrK1XlflJLMzhTM_jaQtqgRxyXyhNQ&s');
+            background-size: cover;
+            background-repeat: no-repeat;
+            color: #333;
+            padding: 20px;
+        }
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 15px rgba(0,0,0,0.1);
+        }
+        h1 {
+            text-align: center;
+            color: #007bff;
+        }
+        .detail {
+            margin-bottom: 15px;
+        }
+        .detail label {
+            font-weight: bold;
+            color: #555;
+        }
+        .detail span {
+            color: #777;
+        }
+    </style>
+</head>
+
+</html>
